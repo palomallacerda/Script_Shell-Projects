63c573242f43fa56161c254cab83e156ab478154
palomallacerda <psls@ic.ufal.br>
2022-08-19 11:48:32 -0300

    update

--- Compressao.sh
+++ Compressao.sh
@@ -19,20 +19,24 @@ main()
    echo "1. Compactar para .tar"
    echo "2. Compactar para .tar.gz"
    echo "3. Compactar para .bz2"
-   echo "4. Descompactar .tar"
-   echo "5. Descompactar .tar.gz"
-   echo "6. Descompactar .bz2"
+   echo "4. Compactar para .zip"
+   echo "5. Descompactar .tar"
+   echo "6. Descompactar .tar.gz"
+   echo "7. Descompactar .bz2"
+   echo "8. Descompactar .zip"
    echo "0. sair"
    echo ""
    echo -n "Sua opção: "
    read OPCAO
-   case $OPCAO in 
+   case $OPCAO in
       1) compactarTar;;
       2) compactarGz;;
       3) compactarBz2;;
-      4) descompactarTar;;
-      5) descompactarGz;;
-      6) descompactarBz2;;
+      4) compactarZip;;
+      5) descompactarTar;;
+      6) descompactarGz;;
+      7) descompactarBz2;;
+      8) descompactarZip;;
       0) exit;;
       *) echo "Opção inválida"; clear; main;;
    esac
@@ -42,19 +46,22 @@ compactarTar()
    clear
    echo -n "Digite o diretorio onde esta o arquivo:"
    read DIRETORIO
-   echo -n "Digite o nome do arquivo: " 
+   echo -n "Digite o nome do arquivo: "
    read ARQUIVO
    COMPACTAR="$DIRETORIO/$ARQUIVO"
 
+   #comando para compactar arquivo tar
    tar -cf $ARQUIVO.tar $COMPACTAR 2> /dev/null
    if [ $? -eq 0 ]; then
       echo "Arquivo compactado"
       sleep 2
       clear
+      exit
    else
             echo "Arquivo não foi compactado"
             clear
-        fi
+   fi
+
 main
 }
 compactarGz()
@@ -71,6 +78,7 @@ compactarGz()
            echo "Arquivo compactado"
            sleep 2
            clear
+	   exit
         else
             echo "Arquivo não foi compactado"
             clear
@@ -89,14 +97,36 @@ compactarBz2()
         tar -cjf $ARQUIVO.bz2 $COMPACTAR 2> /dev/null
         if [ $? -eq 0 ]; then
            echo "Arquivo compactado"
-           sleep 2
+           sleep 3
            clear
+           exit
         else
             echo "Arquivo não foi compactado"
             clear
         fi
 main
 }
+
+compactarZip(){
+	clear
+	echo -n "Digite o diretorio onde esta o arquivo: "
+	read DIRETORIO
+	echo -n "Digite o nome do arquivo: "
+	read ARQUIVO
+	COMPACTAR="$DIRETORIO/$ARQUIVO"
+
+	zip -v $ARQUIVO.zip "~/${COMPACTAR}" 2> /dev/null
+
+	if [ $? -eq 0 ]; then
+	   echo "Arquivo zip compactado"
+	   sleep 3
+	   exit
+	else
+	   echo "Arquivo zip não compactado"
+	  exit
+	fi
+}
+
 descompactarTar()
 {
    clear
@@ -108,13 +138,14 @@ descompactarTar()
    tar -xf $CAMINHO
    if [ $? -eq 0 ]; then
      echo "Arquivo descompactado no diretorio corrente."
+     exit
    else
      echo "Erro! arquivo não foi descompactado"
    fi
-   sleep 2
+   sleep 3
    clear
    main
-}   
+}
 descompactarGz()
 {
         clear
@@ -126,6 +157,7 @@ descompactarGz()
         tar -xzf $CAMINHO
         if [ $? -eq 0 ]; then
           echo "Arquivo descompactado no diretorio corrente."
+	  exit
         else
           echo "Erro! arquivo não foi descompactado"
         fi
@@ -145,10 +177,11 @@ descompactarBz2()
         tar -xjf $CAMINHO
         if [ $? -eq 0 ]; then
           echo "Arquivo descompactado no diretorio corrente."
+	  exit
         else
           echo "Erro! arquivo não foi descompactado"
         fi
-        sleep 2
+        sleep 3
         clear
         main
 }
